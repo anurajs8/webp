db7515834a1a963cd14da50caedecd0bc5ac8992
anurajs8 <anurajab08.com>
2024-01-29 22:14:03 +0000

    Initial commit

---
+++ src/webparts/lab01/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ILab01WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'Lab01WebPartStrings' {
+  const strings: ILab01WebPartStrings;
+  export = strings;
+}
